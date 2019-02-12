8cae9ea171a4cedfeb0cbbb2e689f8adacec4e48
chenkai <752337765@qq.com>
2019-02-12 11:28:33 +0800

    修改对activity的全局管理

--- common/src/main/java/com/android/lib/util/ScreenManager.java
+++ common/src/main/java/com/android/lib/util/ScreenManager.java
@@ -14,17 +14,12 @@ import java.util.Stack;
  */
 public class ScreenManager {
 
-    private static Stack<Activity> activityStack;
-//    private static ScreenManager instance;
+    private static Stack<Activity> activityStack = new Stack<>();
 
     private ScreenManager() {
     }
 
     public static ScreenManager getScreenManager() {
-//        if (instance == null) {
-//            instance = new ScreenManager();
-//        }
-//        return instance;
         return ScreenManagerHolder.sInstance;
     }
 
@@ -62,26 +57,24 @@ public class ScreenManager {
     /**
      * 结束指定类名的Activity
      */
-//	public void killActivity(Class<?> cls) {
-//		for (Activity activity : activityStack) {
-//			if (activity.getClass().equals(cls)) {
-//				killActivity(activity);
-//			}
-//		}
-//	}
+    public void killActivity(Class<?> cls) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass().equals(cls)) {
+                killActivity(activity);
+            }
+        }
+    }
 
     /**
-     * 结束所有Activity
+     * 结束所有栈中的Activity
      */
     public void killAllActivity() {
-        if (activityStack != null) {
-            for (int i = 0, size = activityStack.size(); i < size; i++) {
-                if (null != activityStack.get(i)) {
-                    activityStack.get(i).finish();
-                }
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
             }
-            activityStack.clear();
         }
+        activityStack.clear();
     }
 
     /**
@@ -103,13 +96,9 @@ public class ScreenManager {
      * 弹出栈并finish activity
      */
     public void popActivity() {
-        Activity activity = null;
-        if (activityStack != null) {
-            activity = activityStack.pop();
-            if (activity != null) {
-                activity.finish();
-                activity = null;
-            }
+        Activity activity = activityStack.pop();
+        if (activity != null) {
+            activity.finish();
         }
     }
 
@@ -117,10 +106,9 @@ public class ScreenManager {
      * 只弹出栈但不finish
      */
     public void popActivityNoFinish() {
-        if (activityStack != null) {
-            int size = getSize();
-            if (size > 0)
-                activityStack.pop();
+        int size = getSize();
+        if (size > 0) {
+            activityStack.pop();
         }
     }
 
@@ -130,23 +118,18 @@ public class ScreenManager {
      * @param cls
      */
     public void popAllActivityExceptOne(Class<?> cls) {
-        if (activityStack != null) {
-
-            while (true) {
-                Activity activity = activityStack.pop();
-                if (activity == null) {
-                    break;
-                }
-                if (activity.getClass().equals(cls)) {
-                    activityStack.remove(activity);
-                    activity.finish();
-                    activity = null;
-                    break;
-                }
-//				activity.finish();
-//				activity = null;
+        while (true) {
+            Activity activity = activityStack.pop();
+            if (activity == null) {
+                break;
+            }
+            if (activity.getClass().equals(cls)) {
+                activityStack.remove(activity);
+                activity.finish();
+                break;
             }
         }
+
     }
 
     /**
@@ -163,7 +146,6 @@ public class ScreenManager {
             if (activity.getClass().equals(cls)) {
                 break;
             }
-
         }
     }
 
@@ -171,32 +153,28 @@ public class ScreenManager {
      * 清除并finish释放内存
      */
     public void clearAllActivity() {
-        if (activityStack != null) {
-            int size = getSize();
-            boolean isEmpty = activityStack.isEmpty();
-            while (size > 0 && !isEmpty) {
-                popActivity();
-                isEmpty = activityStack.isEmpty();
-                size--;
-            }
-            activityStack.clear();
+        int size = getSize();
+        boolean isEmpty = activityStack.isEmpty();
+        while (size > 0 && !isEmpty) {
+            popActivity();
+            isEmpty = activityStack.isEmpty();
+            size--;
         }
+        activityStack.clear();
     }
 
     /**
      * 清除栈中的内容，但并不finish释放内存
      */
     public void clearAllNoFinish() {
-        if (activityStack != null) {
-            int size = getSize();
-            boolean isEmpty = activityStack.isEmpty();
-            while (size > 0 && !isEmpty) {
-                popActivityNoFinish();
-                isEmpty = activityStack.isEmpty();
-                size--;
-            }
-            activityStack.clear();
+        int size = getSize();
+        boolean isEmpty = activityStack.isEmpty();
+        while (size > 0 && !isEmpty) {
+            popActivityNoFinish();
+            isEmpty = activityStack.isEmpty();
+            size--;
         }
+        activityStack.clear();
     }
 
     /**
@@ -205,21 +183,14 @@ public class ScreenManager {
      * @param activity
      */
     public void pushActivity(Activity activity) {
-        if (activityStack == null) {
-            activityStack = new Stack<Activity>();
-        }
         activityStack.push(activity);
     }
 
     /**
      * 获得最近的activity
-     *
-     * @return
      */
     public Activity currentActivity() {
-        Activity activity = null;
-        if (activityStack != null)
-            activity = activityStack.lastElement();
+        Activity activity = activityStack.lastElement();
         return activity;
     }
 
@@ -230,9 +201,7 @@ public class ScreenManager {
      */
     public int getSize() {
         int size = 0;
-        if (activityStack != null) {
-            size = activityStack.size();
-        }
+        size = activityStack.size();
         return size;
     }
 
@@ -243,42 +212,59 @@ public class ScreenManager {
      * @return
      */
     public Activity getElement(int i) {
-        Activity activity = null;
-        if (activityStack != null)
-            activity = activityStack.elementAt(i);
+        Activity activity = activityStack.elementAt(i);
         return activity;
     }
 
     /**
      * 界面跳转
      *
-     * @param activity 当前界面
-     * @param intent   启动下个是否压入栈中
+     * @param activity   当前页面
+     * @param intent     目标页面
+     * @param isPutStack 启动下个是否压入栈中
      */
     public void startPage(Activity activity, Intent intent, boolean isPutStack) {
-        if (activity == null || intent == null)
+        if (activity == null || intent == null) {
             return;
-        int requestCode = getSize();
+        }
+        activity.startActivity(intent);
+        if (isPutStack) {
+            pushActivity(activity);
+        } else {
+            activity.finish();
+        }
+    }
+
+    /**
+     * 界面跳转
+     *
+     * @param activity    当前页面
+     * @param intent      目标页面
+     * @param isPutStack  启动下个是否压入栈中
+     * @param requestCode 请求code
+     */
+    public void startPage(Activity activity, Intent intent, boolean isPutStack, int requestCode) {
+        if (activity == null || intent == null) {
+            return;
+        }
         activity.startActivityForResult(intent, requestCode);
         if (isPutStack) {
             pushActivity(activity);
         } else {
             activity.finish();
-            activity = null;
         }
     }
 
     /**
      * 返回上个界面
      *
-     * @return 栈中是后有界面，有返回成功，没有返回失败
+     * @return 栈中是否有界面, 有, 返回true; 没有,返回false
      */
     public boolean goBlackPage() {
         boolean re = false;
         int size = getSize();
         boolean isEmpty = true;
-        if (activityStack != null)
-            isEmpty = activityStack.isEmpty();
+        isEmpty = activityStack.isEmpty();
         if (size > 0 && !isEmpty) {
             popActivityNoFinish();
             re = true;
