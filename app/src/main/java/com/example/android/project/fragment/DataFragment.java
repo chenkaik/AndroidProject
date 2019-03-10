658dfdf5842dbc6d385fbd20096f4ef8d81e7117
chenkai <752337765@qq.com>
2019-03-10 20:53:54 +0800

    测试使用bottomNavigationView下fragment的生命周期

--- app/src/main/java/com/example/android/project/fragment/DataFragment.java
+++ app/src/main/java/com/example/android/project/fragment/DataFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.lib.Logger;
 import com.example.android.project.R;
 
 /**
@@ -16,15 +17,36 @@ import com.example.android.project.R;
  */
 public class DataFragment extends Fragment {
 
+    private static final String TAG = "DataFragment";
+
     public static DataFragment newInstance() {
         return new DataFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Logger.e(TAG, "onCreate: " );
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_data, container, false);
+        Logger.e(TAG, "onCreateView: " );
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Logger.e(TAG, "onResume: " );
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Logger.e(TAG, "onDestroy: " );
     }
 
 }
--- app/src/main/java/com/example/android/project/fragment/HomeFragment.java
+++ app/src/main/java/com/example/android/project/fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.project.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,7 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.lib.Logger;
+import com.android.lib.util.ScreenManager;
 import com.example.android.project.R;
+import com.example.android.project.activity.TestActivity;
 
 /**
  * @date: 2019/3/8
@@ -16,15 +20,43 @@ import com.example.android.project.R;
  */
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "HomeFragment";
+
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Logger.e(TAG, "onCreate: ");
+    }
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        Logger.e(TAG, "onCreateView: ");
+        rootView.findViewById(R.id.tv_home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TestActivity.class);
+                ScreenManager.getScreenManager().startPage(getActivity(), intent, true);
+            }
+        });
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Logger.e(TAG, "onResume: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Logger.e(TAG, "onDestroy: ");
     }
 
 }
