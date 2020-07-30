3ab962abc58bca88bcc34201418249959e380488
chenkai <752337765@qq.com>
2020-07-30 22:25:31 +0800

    BottomNavigationView添加角标

--- app/src/main/java/com/example/android/project/activity/MainActivity.java
+++ app/src/main/java/com/example/android/project/activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.project.activity;
 
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,6 +15,8 @@ import com.example.android.project.fragment.DataFragment;
 import com.example.android.project.fragment.FindFragment;
 import com.example.android.project.fragment.HomeFragment;
 import com.example.android.project.fragment.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
@@ -26,6 +31,7 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
     BottomNavigationView mBottomNavigationView;
 
     private BaseFragmentAdapter<Fragment> mPagerAdapter;
+    private TextView mBadgeView;
 
     @Override
     protected int getLayoutId() {
@@ -38,6 +44,21 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
         mBottomNavigationView.setItemIconTintList(null); // 不使用图标默认变色
 //        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
         mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+//        mBottomNavigationView.setItemIconSize(55); // 设置菜单项图标的大小
+        mBottomNavigationView.getMenu().getItem(1).setChecked(true);
+        // 添加角标
+        // 获取整个的BottomNavigationMenuView
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) mBottomNavigationView.getChildAt(0);
+        // 获取所添加的Tab或者叫Menu
+        View tab = menuView.getChildAt(1);
+        BottomNavigationItemView itemView = (BottomNavigationItemView) tab;
+        // 加载角标View
+        View badge = LayoutInflater.from(this).inflate(R.layout.badge_layout, menuView, false);
+        // 添加到Tab上
+        itemView.addView(badge);
+        mBadgeView = badge.findViewById(R.id.badge_number);
+        mBadgeView.setText("99+");
+//        mBadgeView.setVisibility(View.GONE);
     }
 
     @Override
@@ -49,6 +70,7 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
         mPagerAdapter.addFragment(UserFragment.newInstance());
         mViewPager.setAdapter(mPagerAdapter);
         mViewPager.setOffscreenPageLimit(mPagerAdapter.getCount()); // 限制页面数量
+        mViewPager.setCurrentItem(1);
     }
 
     @Override
@@ -116,5 +138,29 @@ public class MainActivity extends BaseActivity implements ViewPager.OnPageChange
         mPagerAdapter = null;
         super.onDestroy();
     }
+
+    /**
+     * setSelectedItemId(int itemId) 设置选择的菜单项 ID。
+     * setElevation(float elevation) 设置此视图的基本高程（以像素为单位）。
+     * setItemBackground(Drawable background) 将菜单项的背景设置为给定的可绘制对象。
+     * setItemBackgroundResource(int resId) 将菜单项的背景设置为给定资源。
+     * setItemIconSize(int iconSize) 设置大小以提供菜单项图标。
+     * setItemIconTintList(ColorStateList tint) 设置应用于菜单项图标的色彩。
+     * setItemRippleColor(ColorStateList itemRippleColor) 将菜单项的背景设置为具有给定颜色的波纹。
+     * setItemTextAppearanceActive(int textAppearanceRes) 设置用于菜单项标签的文本样式。
+     * setItemTextAppearanceInactive(int textAppearanceRes) 设置用于非活动菜单项标签的文本样式。
+     * setItemTextColor(ColorStateList textColor) 设置颜色以用于菜单项文本的不同状态（正常，选中，聚焦等）。
+     * setLabelVisibilityMode(int labelVisibilityMode) 设置导航项目的标签可见性模式。
+     * setItemHorizontalTranslationEnabled(boolean itemHorizontalTranslationEnabled) 设置当合并的项目宽度填满屏幕时，菜单项是否在选择时水平平移。
+     * setOnNavigationItemReselectedListener(BottomNavigationView.OnNavigationItemReselectedListener listener) 设置一个侦听器，当重新选择当前选择的底部导航项时将通知该侦听器。
+     * setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener listener) 设置一个侦听器，当选择底部导航项时将通知该侦听器。
+     * getMenu() 返回 Menu 与此底部导航栏关联的实例。
+     * getMaxItemCount() 返回最大 Menu 数量。
+     *
+     * 作者：Jaynm
+     * 链接：https://juejin.im/post/5f1fed2cf265da22d26bb13a
+     * 来源：掘金
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
 
 }
